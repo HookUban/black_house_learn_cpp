f970e597ce66a737aac64e65063030c17d489387
HookUban <2122484643@qq.com>
2025-06-24 23:31:27 +0800

    递增递减运算符重载

--- class_object_object_properties/运算符重载.cpp
+++ class_object_object_properties/运算符重载.cpp
@@ -81,6 +81,7 @@ int main()
 }*/
 
 
+/*
 // 左移运算符重载
 
 class Person
@@ -123,5 +124,96 @@ void test01()
 int main()
 {
     test01();
+    return 0;
+}*/
+
+
+// 递增运算符重载
+
+// 自定义的整型
+class MyInteger
+{
+    friend ostream& operator<<(ostream& cout, MyInteger myint);
+public:
+    MyInteger()
+    {
+        m_Num = 0;
+    }
+
+    // 重载前置++运算符
+    MyInteger& operator++()
+    {
+        m_Num++;
+        return *this;
+    }
+
+    // 重载后置++运算符
+
+    MyInteger operator++(int) // int 代表占位参数，可以用于区分前置和后置++运算符
+    {
+        MyInteger temp = *this;
+        m_Num++;
+        return temp;
+    }
+
+    // 重载前置--运算符
+    MyInteger& operator--()
+    {
+        m_Num--;
+        return *this;
+    }
+
+
+    // 重载后置--运算符
+    MyInteger operator--(int)
+    {
+        MyInteger temp = *this;
+        m_Num--;
+        return temp;
+    }
+private:
+    int m_Num;
+};
+
+// 重载一下左移运算符
+ostream& operator<<(ostream& cout, MyInteger myint)
+{
+    cout << myint.m_Num;
+    return cout;
+}
+
+void test01()
+{
+    MyInteger myint;
+    cout << ++(++myint) << endl;
+    cout << myint << endl;
+}
+
+
+void test02()
+{
+    MyInteger myint;
+    cout << myint++ << endl;
+    cout << myint << endl;
+
+}
+
+void test03()
+{
+    MyInteger myint;
+    cout << --(--myint) << endl;
+    cout << myint << endl;
+
+    cout << myint-- << endl;
+    cout << myint << endl;
+
+}
+
+
+int main()
+{
+//    test01();
+//    test02();
+    test03();
     return 0;
 }

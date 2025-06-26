6aa5682f071f27c25f7ca06d9c4e05a0ece41200
HookUban <2122484643@qq.com>
2025-06-26 10:53:07 +0800

    vs的.gitignore文件

---
+++ 基于多态的职工管理系统/基于多态的职工管理系统/职工管理系统.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+
+
+/**
+
+管理类负责的内容如下：
+	与用户的沟通菜单界面
+	对职工增删改查的操作
+	与文件的读写交互
+
+*/
+
+#include "workerManager.h"
+
+int main()
+{
+
+	// 实例化管理者对象
+	WorkerManager wm;
+
+	int choice = 0; // 用户选择
+
+	while (true)
+	{
+		// 调用展示菜单的成员函数
+		wm.Show_Menu();
+		cout << "请输入您的选择:" << endl;
+		cin >> choice;
+
+		switch (choice)
+		{
+		case 0: // 退出系统
+			wm.ExitSystem();
+			break;
+
+		case 1:	// 增加职工
+			break;
+
+		case 2: // 显示职工
+			break;
+		case 3: // 删除职工
+			break;
+		case 4: // 修改职工
+			break;
+		case 5: // 查找职工
+			break;
+		case 6: // 排序职工
+			break;
+		case 7: // 清空文档
+			break;
+		default:
+			system("cls");
+
+		}
+	}
+
+
+	system("pause");
+	return 0;
+}
+
+

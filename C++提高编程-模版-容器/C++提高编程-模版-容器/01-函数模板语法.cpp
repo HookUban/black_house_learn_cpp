90e525b0be4943a2c9b5b215185eadfb289cedf3
HookUban <2122484643@qq.com>
2025-06-26 19:57:47 +0800

    函数模板注意事项

--- C++提高编程-模版-容器/C++提高编程-模版-容器/01-函数模板语法.cpp
+++ C++提高编程-模版-容器/C++提高编程-模版-容器/01-函数模板语法.cpp
@@ -30,30 +30,36 @@ void mySwap(T& a, T& b)
 	b = temp;
 }
 
-int main()
-{
-	int a = 10;
-	int b = 20;
-	cout << a << " " << b << endl;
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	cout << a << " " << b << endl;
+//
+//	//swapInt(a, b);
+//	// 利用函数模板交换
+//	// 两种方式使用函数模板
+//	// 1.自动类型推导
+//	//mySwap(a, b);
+//	// 2.显示指定类型
+//	mySwap<int>(a, b); // 显示指定类型
+//	cout << a << " " << b << endl;
+//
+//	//double c = 1.1;
+//	//double d = 2.2;
+//	//cout << c << " " << d << endl;
+//	//swapDouble(c, d);
+//	//cout << c << " " << d << endl;
+//
+//
+//
+//
+//	system("pause");
+//	return 0;
+//}
+/*
+函数模板利用关键字template
+使用函数模板有两种方式：自动类型推导、显示指定类型
+模板的目的是为了提高复用性，将类型参数化
 
-	//swapInt(a, b);
-	// 利用函数模板交换
-	// 两种方式使用函数模板
-	// 1.自动类型推导
-	//mySwap(a, b);
-	// 2.显示指定类型
-	mySwap<int>(a, b); // 显示指定类型
-	cout << a << " " << b << endl;
-
-	//double c = 1.1;
-	//double d = 2.2;
-	//cout << c << " " << d << endl;
-	//swapDouble(c, d);
-	//cout << c << " " << d << endl;
-
-
-
-
-	system("pause");
-	return 0;
-}
+*/

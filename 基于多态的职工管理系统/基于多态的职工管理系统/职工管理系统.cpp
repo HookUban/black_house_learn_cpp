3a89e48879b4ff98338be593654ab9aa1c6f00ed
HookUban <2122484643@qq.com>
2025-06-26 11:37:59 +0800

    测试普通员工、经理、老板类多态实现

--- 基于多态的职工管理系统/基于多态的职工管理系统/职工管理系统.cpp
+++ 基于多态的职工管理系统/基于多态的职工管理系统/职工管理系统.cpp
@@ -13,55 +13,71 @@ using namespace std;
 */
 
 #include "workerManager.h"
-#include "worker.h"
-#include "employee.h"
+ 
+//#include "worker.h"
+//#include "employee.h"
+//#include "boss.h"
+//#include "manager.h"
+
 
 
 int main()
 {
-	// 测试代码
-	Worker* worker = new Employee(1, "张三", 1);
-	worker->showInfo();
+	//// 测试代码
+	//Worker* worker = NULL;
+	//worker = new Employee(1, "张三", 1);
+	//worker->showInfo();
+	//delete worker;
+
+	//worker = new Manager(2, "李四", 2);
+	//worker->showInfo();
+	//delete worker;
+
+	//worker = new Boss(3, "王五", 3);
+	//worker->showInfo();
+	//delete worker;
 
 
-	//// 实例化管理者对象
-	//WorkerManager wm;
 
-	//int choice = 0; // 用户选择
 
-	//while (true)
-	//{
-	//	// 调用展示菜单的成员函数
-	//	wm.Show_Menu();
-	//	cout << "请输入您的选择:" << endl;
-	//	cin >> choice;
+	// 实例化管理者对象
+	WorkerManager wm;
 
-	//	switch (choice)
-	//	{
-	//	case 0: // 退出系统
-	//		wm.ExitSystem();
-	//		break;
+	int choice = 0; // 用户选择
 
-	//	case 1:	// 增加职工
-	//		break;
+	while (true)
+	{
+		// 调用展示菜单的成员函数
+		wm.Show_Menu();
+		cout << "请输入您的选择:" << endl;
+		cin >> choice;
 
-	//	case 2: // 显示职工
-	//		break;
-	//	case 3: // 删除职工
-	//		break;
-	//	case 4: // 修改职工
-	//		break;
-	//	case 5: // 查找职工
-	//		break;
-	//	case 6: // 排序职工
-	//		break;
-	//	case 7: // 清空文档
-	//		break;
-	//	default:
-	//		system("cls");
+		switch (choice)
+		{
+		case 0: // 退出系统
+			wm.ExitSystem();
+			break;
 
-	//	}
-	//}
+		case 1:	// 增加职工
+			break;
+
+		case 2: // 显示职工
+			break;
+		case 3: // 删除职工
+			break;
+		case 4: // 修改职工
+			break;
+		case 5: // 查找职工
+			break;
+		case 6: // 排序职工
+			break;
+		case 7: // 清空文档
+			break;
+		default:
+			system("cls");
+
+		}
+	}
 
 
 	system("pause");

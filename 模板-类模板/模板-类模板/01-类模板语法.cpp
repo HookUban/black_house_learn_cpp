cc02c28a04c3123c56a44626f1e8e5354bcf5125
HookUban <2122484643@qq.com>
2025-06-27 09:46:49 +0800

    类模板和函数模板的区别

--- 模板-类模板/模板-类模板/01-类模板语法.cpp
+++ 模板-类模板/模板-类模板/01-类模板语法.cpp
@@ -1,39 +1,44 @@
-#include <iostream>
-using namespace std;
-#include <string>
-
-// 类模板
-template <class NameType, class AgeType>
-class Person
-{
-public:
-	Person(NameType name ,AgeType age)
-	{
-		this->m_Name = name;
-		this->m_Age = age;
-	}
-	void showPerson()
-	{
-		cout << "name : "<<this->m_Name << endl;
-		cout << "age: " << this->m_Age << endl;
-	}
-
-	NameType m_Name;
-	AgeType m_Age;
-};
-
-void test01()
-{
-	Person<string, int> p1("孙悟空",999);
-	p1.showPerson();	
-
-}
-
-
-
-int main()
-{
-	test01();
-	system("pause");
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//#include <string>
+//
+//// 类模板
+//template <class NameType, class AgeType>
+//class Person
+//{
+//public:
+//	Person(NameType name ,AgeType age)
+//	{
+//		this->m_Name = name;
+//		this->m_Age = age;
+//	}
+//	void showPerson()
+//	{
+//		cout << "name : "<<this->m_Name << endl;
+//		cout << "age: " << this->m_Age << endl;
+//	}
+//
+//	NameType m_Name;
+//	AgeType m_Age;
+//};
+//
+//void test01()
+//{
+//	Person<string, int> p1("孙悟空",999);
+//	p1.showPerson();	
+//
+//}
+//
+//
+//
+//int main()
+//{
+//	test01();
+//	system("pause");
+//	return 0;
+//}
+//
+///*
+//类模板和函数模板十分相似，在声明模板template后面加类，次类称为类模板
+//
+//*/

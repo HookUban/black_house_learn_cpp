14cc4708ae0610163257b9678f79ec3c2d44920d
HookUban <2122484643@qq.com>
2025-06-27 16:34:13 +0800

    string查找和替换

--- string容器/string容器/03-string容器字符串拼接.cpp
+++ string容器/string容器/03-string容器字符串拼接.cpp
@@ -1,38 +1,38 @@
-#include <iostream>
-using namespace std;
-#include <string>
-
-
-void test01()
-{
-
-	string str1 = "ÎÒ";
-	str1 += "°®Äã";
-
-	cout << str1 << endl;
-	str1 += '!';
-	cout << str1 << endl;
-
-	string str2 = "Äã°®ÎÒÂğ";
-	str1 += str2;
-
-	cout << str1 << endl;
-
-
-	string str3 = "I";
-	str3.append(" love");
-	cout << "str3 = " << str3 << endl;
-	str3.append(" you abcdef", 4);
-
-	cout << "str3 = " << str3 << endl;
-
-
-}
-
-
-int main()
-{
-	test01();
-	system("pause");
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//#include <string>
+//
+//
+//void test01()
+//{
+//
+//	string str1 = "ÎÒ";
+//	str1 += "°®Äã";
+//
+//	cout << str1 << endl;
+//	str1 += '!';
+//	cout << str1 << endl;
+//
+//	string str2 = "Äã°®ÎÒÂğ";
+//	str1 += str2;
+//
+//	cout << str1 << endl;
+//
+//
+//	string str3 = "I";
+//	str3.append(" love");
+//	cout << "str3 = " << str3 << endl;
+//	str3.append(" you abcdef", 4);
+//
+//	cout << "str3 = " << str3 << endl;
+//
+//
+//}
+//
+//
+//int main()
+//{
+//	test01();
+//	system("pause");
+//	return 0;
+//}
---
+++ string容器/string容器/04-string容器字符串查找和替换.cpp
@@ -0,0 +1,42 @@
+//#include <iostream>
+//using namespace std;
+//#include <string>
+//
+//
+//
+//void test01()
+//{
+//	string str1 = "abcdefg";
+//	int ret = str1.find("de");
+//
+//	if (ret == -1)
+//	{
+//		cout << "Î´ÕÒµ½×Ö·û´®" << endl;
+//	}
+//	else
+//	{
+//		cout << "ÕÒµ½×Ö·û´®£¬ pos = " << ret << endl;
+//	}
+//	// rfind
+//	ret = str1.rfind("de");
+//	cout << "ret = " << ret << endl;
+//	// rfindµÄºÍfindµÄÇø±ð
+//	// rfind´ÓÓÒÍù×ó²éÕÒ£¬find´Ó×óÍùÓÒ²éÕÒ
+//
+//}
+//
+//void test02()
+//{
+//	string str1 = "abcdefg";
+//	cout << str1 << endl;
+//	str1.replace(1, 3, "11111");
+//	cout << str1 << endl;
+//}
+//
+//int main()
+//{
+//	//test01();
+//	test02();
+//	system("pause");
+//	return 0;
+//}

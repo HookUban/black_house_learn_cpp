3260f47b3ef6b125b1f93d60ce886653b886d259
HookUban <2122484643@qq.com>
2025-06-27 14:58:00 +0800

    完成数组类封装

--- 模板-类模板/模板-类模板/MyArray.hpp
+++ 模板-类模板/模板-类模板/MyArray.hpp
@@ -64,6 +64,46 @@ public:
 		return *this;
 	}
 
+	// 尾插法
+	void Push_Back(const T& val)
+	{
+		// 判断容量是否等于大小
+		if (this->m_Capacity == this->m_Size)
+		{
+			return;
+		}
+		this->pAddress[this->m_Size] = val;
+		this->m_Size++;// 更新数组大小
+	}
+
+	// 尾删法
+	void Pop_Back()
+	{
+		// 让用户访问不到最后一个元素，逻辑删除
+		if (this->m_Size == 0)
+		{
+			return;
+		}
+		this->m_Size--;
+	}
+
+	// 通过下标方式来访问数组中的元素
+	T& operator[](int index)
+	{
+		return this->pAddress[index];
+	}
+
+	// 返回数组的容量 
+	int getCapacity()
+	{
+		return this->m_Capacity;
+	}
+	// 返回数组的大小
+	
+	int getSize()
+	{
+		return this->m_Size;
+	}
 	 // 析构函数
 	~MyArray()
 	{

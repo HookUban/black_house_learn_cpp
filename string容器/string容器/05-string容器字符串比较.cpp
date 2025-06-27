5612f21b1736fe34d514a8a335197677a2fd56fd
HookUban <2122484643@qq.com>
2025-06-27 16:42:19 +0800

    string字符串的比较

---
+++ string容器/string容器/05-string容器字符串比较.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+
+void test01()
+{
+	string str1 = "xello";
+	string str2 = "hello";
+
+	if (str1.compare(str2) == 0)
+	{
+		cout << "str1 等于 str2" << endl;
+	}
+	else if (str1.compare(str2) > 0)
+	{
+		cout << "str1大于str2" << endl;
+	}
+	else if (str1.compare(str2) < 0)
+	{
+		cout << "str1小于str2" << endl;
+	}
+
+	
+
+}
+
+int main()
+{
+	test01();
+	system("pause");
+	return 0;
+}

b1cf99c6fb8fef12d155880e2da082184b23d6d2
HookUban <2122484643@qq.com>
2025-06-27 11:43:59 +0800

    类模板与友元

--- 模板-类模板/模板-类模板/07-类模板分文件编写.cpp
+++ 模板-类模板/模板-类模板/07-类模板分文件编写.cpp
@@ -1,47 +1,52 @@
-#include <iostream>
-#include <string>
-// 第一种解决方式直接包含源文件
-//#include "person.cpp"
-// 第二种解决方式，将.h和.cpp中的1内容写到一起，将后缀改为.hpp 
-#include "person.hpp"
-using namespace std;
-
-// 类模板分文件编写问题以及解决
-
-//template <class T1, class T2>
-//class Person
-//{
-//public:
-//	Person(T1 name, T2 age);
+//#include <iostream>
+//#include <string>
+//// 第一种解决方式直接包含源文件
+////#include "person.cpp"
+//// 第二种解决方式，将.h和.cpp中的1内容写到一起，将后缀改为.hpp 
+//#include "person.hpp"
+//using namespace std;
 //
-//	void showPerson();
-//	T1 m_Name;
-//	T2 m_Age;
-//};
-
-//template <class T1, class T2>
-//Person<T1, T2>::Person(T1 name, T2 age)
+//// 类模板分文件编写问题以及解决
+//
+////template <class T1, class T2>
+////class Person
+////{
+////public:
+////	Person(T1 name, T2 age);
+////
+////	void showPerson();
+////	T1 m_Name;
+////	T2 m_Age;
+////};
+//
+////template <class T1, class T2>
+////Person<T1, T2>::Person(T1 name, T2 age)
+////{
+////	this->m_Name = name;
+////	this->m_Age = age;
+////}
+////
+////template <class T1, class T2>
+////void Person<T1, T2>::showPerson()
+////{
+////	cout << "姓名是:" << this->m_Name << " 年龄是:" << this->m_Age << endl;
+////
+////}
+//
+//void test01()
 //{
-//	this->m_Name = name;
-//	this->m_Age = age;
+//	Person<string, int> p("jerry", 19);
+//	p.showPerson();
 //}
 //
-//template <class T1, class T2>
-//void Person<T1, T2>::showPerson()
+//int main()
 //{
-//	cout << "姓名是:" << this->m_Name << " 年龄是:" << this->m_Age << endl;
-//
+//	test01();
+//	system("pause");
+//	return 0;
 //}
-
-void test01()
-{
-	Person<string, int> p("jerry", 19);
-	p.showPerson();
-}
-
-int main()
-{
-	test01();
-	system("pause");
-	return 0;
-}
+///*
+//主流的结局方式是第二种，将类模板成员函数写到一起，并将后缀名改为.hpp
+//
+//
+//*/
---
+++ 模板-类模板/模板-类模板/08-类模板与友元.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+
+// 提前让编译器知道Person类的存在
+
+template <class T1, class T2>
+class Person;
+
+// 类外实现
+template <class T1, class T2>
+void printPerson2(Person<T1, T2> p)
+{
+	cout << "类外实现   姓名：" << p.m_Name << "年龄：" << p.m_Age << endl;
+}
+
+
+template <class T1, class T2>
+class Person
+{
+
+	// 全局函数 类内实现
+	friend void printPerson(Person<T1, T2> p)
+	{
+		cout << "姓名：" << p.m_Name << "年龄：" << p.m_Age << endl;
+
+	}
+	// 全局函数 类外实现
+	// 加一个空模板参数列表
+	// 如果是全局函数，是类外实现，需要让编译器提前知道之歌函数的存在
+
+	friend void printPerson2<>(Person<T1, T2> p);
+
+public:
+	Person(T1 name, T2 age)
+	{
+		this->m_Name = name;
+		this->m_Age = age;
+	}
+private:
+	T1 m_Name;
+	T2 m_Age; 
+};
+
+
+// 1.全局函数在类内实现
+void test01()
+{
+	Person<string, int> p("Tom", 19);
+	printPerson(p);
+}
+
+
+// 2.全局函数在类外实现
+void test02()
+{
+	Person<string, int> p("Jerry", 20);
+	printPerson2(p);
+}
+
+int main() {
+
+	//test01();
+	test02();
+	system("pause");
+	return 0;
+}

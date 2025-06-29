fe94672746cda0bcf743d5c0f91b58d8e84a92c3
HookUban <2122484643@qq.com>
2025-06-29 23:23:11 +0800

    set容器-查找和统计

---
+++ set容器/set容器/04-set容器-查找和统计.cpp
@@ -0,0 +1,53 @@
+//#include <iostream>
+//using namespace std;
+//
+//#include <set>
+//
+//
+//void test01()
+//{
+//	// 查找
+//	set<int> s1;
+//	// 插入数据
+//	s1.insert(10);
+//	s1.insert(20);
+//	s1.insert(30);
+//	s1.insert(40);
+//
+//	set<int>::iterator pos = s1.find(30);
+//
+//	if (pos != s1.end())
+//	{
+//		cout << "找到了" << endl;
+//	}
+//	else
+//	{
+//		cout << "未找到" << endl;
+//	}
+//
+//
+//
+//
+//}
+//
+//void test02()
+//{
+//	set<int> s1;
+//	s1.insert(10);
+//	s1.insert(20);
+//	s1.insert(30);
+//	s1.insert(40);
+//	int num = s1.count(30);
+//	cout << "num:" << num << endl;
+//
+//
+//}
+//
+//int main()
+//{
+//	//test01();
+//	test02();
+//
+//	system("pause");
+//	return 0;
+//}

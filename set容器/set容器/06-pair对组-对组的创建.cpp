2d208fcac72d404a132c22b486003603d240f792
HookUban <2122484643@qq.com>
2025-06-29 23:35:59 +0800

    对组的创建

---
+++ set容器/set容器/06-pair对组-对组的创建.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//using namespace std;
+//
+//void test01()
+//{
+//	pair<string, int>p("Tom", 20);
+//	cout << "姓名" << p.first << " 年龄：" << p.second << endl;
+//
+//	// 2
+//	pair<string, int> p2 = make_pair("Jerry", 20);
+//	cout << "姓名" << p.first << " 年龄：" << p.second << endl;
+//}
+//
+//
+//int main()
+//{
+//	test01();
+//	system("pause");
+//	return 0;
+//}

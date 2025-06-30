207a18a543ee0535301626f2f38b6c301a33f693
HookUban <2122484643@qq.com>
2025-06-30 10:41:23 +0800

    算术仿函数

---
+++ 函数对象/函数对象/04-内建的函数对象-算术仿函数.cpp
@@ -0,0 +1,36 @@
+//#include <iostream>
+//using namespace std;
+//#include <functional> // 内建的函数对象头文件
+//
+//
+//
+//// 内建函数对象 算术仿函数
+//// negate 一元仿函数 取反仿函数
+//
+//// plus 一元仿函数 取反仿函数
+//
+//void test01()
+//{
+//	negate<int>n;
+//	
+//	cout << n(50) << endl;
+//}
+//
+//
+//void test02()
+//{
+//	plus<int> p;
+//	cout << p(10, 2);
+//
+//}
+//
+//
+//
+//int main()
+//{
+//	//test01();
+//	test02();
+//
+//	system("pause");
+//	return 0;
+//}

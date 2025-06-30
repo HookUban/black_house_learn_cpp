3bc7416acf38ea8bae1008809c43f554ccab1f6d
HookUban <2122484643@qq.com>
2025-06-30 14:29:58 +0800

    常用的排序算法-random_shuffle

---
+++ STL-常用算法/STL-常用算法/10-常用的排序算法-random_shuffle.cpp
@@ -0,0 +1,35 @@
+//#include <iostream>
+//using namespace std;
+//#include <vector>
+//#include <algorithm>
+//
+//
+//void myPrint(int val)
+//{
+//	cout << val << " ";
+//}
+//void test01()
+//{
+//	srand((unsigned int)time(NULL));
+//	vector<int> v;
+//	for (int i = 0; i < 10; i++)
+//	{
+//		v.push_back(i);
+//	}
+//
+//	// ÀûÓÃÏ´ÅÆËã·¨
+//	random_shuffle(v.begin(), v.end());
+//	for_each(v.begin(), v.end(), myPrint);
+//
+//}
+//
+//
+//int main()
+//{
+//	
+//
+//	test01();
+//
+//	system("pause");
+//	return 0;
+//}

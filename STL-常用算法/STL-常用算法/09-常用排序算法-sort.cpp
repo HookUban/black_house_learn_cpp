661c66ab77b5f4536b684d8b83d663ff6be64cba
HookUban <2122484643@qq.com>
2025-06-30 14:24:02 +0800

    常用排序算法-sort

---
+++ STL-常用算法/STL-常用算法/09-常用排序算法-sort.cpp
@@ -0,0 +1,45 @@
+//#include <iostream>
+//using namespace std;
+//#include <algorithm>
+//#include <vector>
+//#include <functional>
+//
+//
+//
+//
+//void myPrint(int val)
+//{
+//	cout << val << " ";
+//}
+//
+//
+//void test01()
+//{
+//	vector<int>v;
+//	v.push_back(10);
+//	v.push_back(30);
+//	v.push_back(50);
+//	v.push_back(20);
+//	v.push_back(40);
+//
+//	sort(v.begin(), v.end());
+//
+//	for_each(v.begin(), v.end(), myPrint);
+//	cout << endl;
+//	// ¸Ä±äÎª½µÐò
+//	sort(v.begin(), v.end(), greater<int>());
+//	for_each(v.begin(), v.end(), myPrint);
+//	cout << endl;
+//
+//
+//}
+//
+//
+//
+//int main()
+//{
+//	test01();
+//
+//	system("pause");
+//	return 0;
+//}

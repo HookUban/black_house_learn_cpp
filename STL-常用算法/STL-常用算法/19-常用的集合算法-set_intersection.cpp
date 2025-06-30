8c7cf0f88f331963f489cd6e63d37607d0a092dc
HookUban <2122484643@qq.com>
2025-06-30 15:22:32 +0800

    常用集合算法-set_intersection

---
+++ STL-常用算法/STL-常用算法/19-常用的集合算法-set_intersection.cpp
@@ -0,0 +1,42 @@
+//#include <iostream>
+//using namespace std;
+//#include <vector>
+//#include <algorithm>
+//
+//void myprint(int val)
+//{
+//	cout << val << " ";
+//}
+//
+//void test01()
+//{
+//	vector<int> v1;
+//	vector<int> v2;
+//	for (int i = 0; i < 10; i++)
+//	{
+//		v1.push_back(i);
+//		v2.push_back(i+5);
+//	}
+//
+//
+//	vector<int> vTarget;
+//
+//	// 目标容器开辟空间
+//	vTarget.resize(min(v1.size(), v2.size()));
+//
+//	// 获取交集
+//	vector<int>::iterator itEnd = set_intersection(v1.begin(), v1.end(), v2.begin(), v2.end(), vTarget.begin());
+//
+//	for_each(vTarget.begin(), itEnd, myprint);
+//
+//	cout << endl;
+//}
+//
+//
+//int main()
+//{
+//	test01();
+//
+//	system("pause");
+//	return 0;
+//}

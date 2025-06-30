a39270dd8f5e8c173175f2352323b7a6cd76a490
HookUban <2122484643@qq.com>
2025-06-30 15:13:14 +0800

    常用算术生成算法-fill

---
+++ STL-常用算法/STL-常用算法/18-常用算术生成算法-fill.cpp
@@ -0,0 +1,37 @@
+//#include <iostream>
+//using namespace std;
+//#include <vector>
+//#include <numeric>
+//#include <algorithm>
+//
+//
+//void myprint(int val)
+//{
+//	cout << val << " ";
+//}
+//
+//void test01()
+//{
+//	vector<int> v;
+//	v.resize(10);
+//
+//	// 后期从新填充
+//	fill(v.begin(), v.end(), 100);
+//
+//	for_each(v.begin(), v.end(), myprint);
+//
+//	cout << endl;
+//
+//}
+//
+//
+//
+//int main()
+//{
+//
+//	test01();
+//
+//	system("pause");
+//	return 0;
+//
+//}

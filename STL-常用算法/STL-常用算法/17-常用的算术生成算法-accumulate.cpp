7e3114b21f4b6dc839db12a4ae5a033dde0df908
HookUban <2122484643@qq.com>
2025-06-30 15:08:54 +0800

    常用算术生成算法-accumulate

---
+++ STL-常用算法/STL-常用算法/17-常用的算术生成算法-accumulate.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+//using namespace std;
+//#include <vector>
+//#include <numeric>
+//
+//
+//void test01()
+//{
+//
+//	vector<int> v;
+//
+//	for (int i = 0; i <= 100; i++)
+//	{
+//		v.push_back(i);
+//	}
+//
+//	int total = accumulate(v.begin(), v.end(), 0);
+//	cout << "total " << total << endl;
+//
+//
+//}
+//
+//
+//
+//int main()
+//{
+//	test01();
+//	system("pause");
+//	return 0;
+//}

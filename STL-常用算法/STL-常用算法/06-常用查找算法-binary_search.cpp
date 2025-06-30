87dc4eb3d66c9dfcb67f701b53e07e4e7e857252
HookUban <2122484643@qq.com>
2025-06-30 11:46:39 +0800

    binary_search

---
+++ STL-常用算法/STL-常用算法/06-常用查找算法-binary_search.cpp
@@ -0,0 +1,37 @@
+//#include <iostream>
+//using namespace std;
+//#include<vector>
+//#include <algorithm>
+//
+//void test01()
+//{
+//	vector<int>v;
+//	for (int i = 0; i < 10; i++)
+//	{
+//		v.push_back(i);
+//	}
+//	// 必须是有序的序列
+//	bool ret = binary_search(v.begin(), v.end(), 9);
+//	if (ret)
+//	{
+//		cout << "找到了" << endl;
+//	}
+//	else
+//	{
+//		cout << "没找到" << endl;
+//	}
+//
+//
+//
+//}
+//
+//
+//int main()
+//{
+//	test01();
+//	system("pause");
+//	return 0;
+//
+//}
+//
+//

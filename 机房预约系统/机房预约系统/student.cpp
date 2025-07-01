5c5e45313b3274268276c79fc55edc02f1a71491
HookUban <2122484643@qq.com>
2025-07-02 00:39:13 +0800

    显示所有预约情况

--- 机房预约系统/机房预约系统/student.cpp
+++ 机房预约系统/机房预约系统/student.cpp
@@ -90,7 +90,7 @@ void Student::applyOrder()
 	{
 		cout << vCom[i].m_ComId << "号机房容量：" << vCom[i].m_MaxNum << endl;
 	}
-	
+
 	while (true)
 	{
 		cin >> room;
@@ -104,7 +104,7 @@ void Student::applyOrder()
 
 	ofstream ofs;
 	ofs.open(ORDER_FILE, ios::app);
-	
+
 	ofs << "date:" << date << " ";
 	ofs << "interval:" << interval << " ";
 	ofs << "stuId:" << this->m_Id << " ";
@@ -157,7 +157,7 @@ void Student::showMyOrder()
 			cout << status << endl;
 		}
 
-	
+
 	}
 	system("pause");
 	system("cls");
@@ -166,7 +166,46 @@ void Student::showMyOrder()
 // 查看所有预约
 void Student::showAllOrder()
 {
+	OrderFile of;
+	if (of.m_Size == 0)
+	{
+		cout << "无预约记录" << endl;
+		system("pause");
+		system("cls");
+	}
 
+	for (int i = 0; i < of.m_Size; i++)
+	{
+		cout << i + 1 << "、";
+		cout << "预约日期：周" << of.m_orderData[i]["date"];
+		cout << " 时间段：" << (of.m_orderData[i]["interval"] == "1" ? "上午" : "下午");
+		cout << " 学号：" << of.m_orderData[i]["stuId"];
+		cout << " 姓名：" << of.m_orderData[i]["stuName"];
+		cout << " 机房编号：" << of.m_orderData[i]["roomId"];
+
+		string status = " 状态:";
+		// 1 审核中 2 已预约 -1预约失败 0取消与预期
+
+		if (of.m_orderData[i]["status"] == "1")
+		{
+			status += "审核中";
+		}
+		else if (of.m_orderData[i]["status"] == "2")
+		{
+			status += "预约成功";
+		}
+		else if (of.m_orderData[i]["status"] == "-1")
+		{
+			status += "预约失败 审核未通过";
+		}
+		else
+		{
+			status += "预约已取消";
+		}
+		cout << status << endl;
+	}
+	system("pause");
+	system("cls");
 }
 // 取消预约
 void Student::cancelOrder()

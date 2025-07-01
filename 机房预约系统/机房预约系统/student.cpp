bafa7e0dd200bd1c29d96359bf9510e59226e0ef
HookUban <2122484643@qq.com>
2025-07-02 00:29:49 +0800

    学生显示自身预约功能实现

--- 机房预约系统/机房预约系统/student.cpp
+++ 机房预约系统/机房预约系统/student.cpp
@@ -121,6 +121,46 @@ void Student::applyOrder()
 void Student::showMyOrder()
 {
 	OrderFile of;
+	if (of.m_Size == 0)
+	{
+		cout << "无预约记录" << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
+
+	for (int i = 0; i < of.m_Size; i++)
+	{
+		if (this->m_Id == atoi(of.m_orderData[i]["stuId"].c_str()))
+		{
+			cout << "预约日期：周" << of.m_orderData[i]["date"];
+			cout << "时间段：" << (of.m_orderData[i]["interval"] == "1" ? "上午" : "下午");
+			cout << "机房编号：" << of.m_orderData[i]["roomId"];
+			string status = "状态:";
+			// 1 审核中 2 已预约 -1 预约失败 0 取消预约
+			if (of.m_orderData[i]["status"] == "1")
+			{
+				status += "审核中";
+			}
+			else if (of.m_orderData[i]["status"] == "2")
+			{
+				status += "预约成功";
+			}
+			else if (of.m_orderData[i]["status"] == "-1")
+			{
+				status += "预约失败 审核未通过";
+			}
+			else
+			{
+				status += "预约已取消";
+			}
+			cout << status << endl;
+		}
+
+	
+	}
+	system("pause");
+	system("cls");
 
 }
 // 查看所有预约

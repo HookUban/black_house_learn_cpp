b57057e31157c79e6f253efbc820169e2e408935
HookUban <2122484643@qq.com>
2025-07-02 09:33:04 +0800

    学生取消预约功能实现

--- 机房预约系统/机房预约系统/student.cpp
+++ 机房预约系统/机房预约系统/student.cpp
@@ -210,5 +210,69 @@ void Student::showAllOrder()
 // 取消预约
 void Student::cancelOrder()
 {
+	OrderFile of;
+	if (of.m_Size == 0)
+	{
+		cout << "无预约记录" << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
+	cout << "审核中或预约成功的记录可以取消，请输入取消的记录" << endl;
+	vector<int> v;	  // 存放在最大容器中的下标编号
 
+	int index = 1;
+	for (int i = 0; i < of.m_Size; i++)
+	{
+		// 先判断是自身的学号
+		if (this->m_Id == atoi(of.m_orderData[i]["stuId"].c_str()))
+		{
+			// 再筛选状态 审核中或预约成功
+			if (of.m_orderData[i]["status"] == "1" || of.m_orderData[i]["status"] == "2")
+			{
+				v.push_back(i);
+				cout << index++ << "、";
+				cout << "预约日期：周" << of.m_orderData[i]["date"];
+				cout << " 时间段：" << (of.m_orderData[i]["interval"] == "1" ? "上午" : "下午");
+				cout << " 机房编号：" << of.m_orderData[i]["roomId"];
+				string status = "状态：";
+				if (of.m_orderData[i]["status"] == "1")
+				{
+					status += "审核中";
+				}
+				else if (of.m_orderData[i]["status"] == "2")
+				{
+					status += "预约成功";
+				}
+				cout << status << endl;
+
+			}
+
+		}
+	}
+	cout << "请输入取消的记录，0代表返回" << endl;
+	int select = 0;
+	while (true)
+	{
+		cin >> select;
+		if (select >= 0 && select <= v.size())
+		{
+			if (select == 0)
+			{
+				break;
+			}
+			else
+			{
+				of.m_orderData[v[select - 1]]["status"] = "0";
+				of.updateOrder();
+
+				cout << "已取消预约" << endl;
+				break;
+			}
+		}
+		cout << "输入有误，请重新输入" << endl;
+	}
+
+	system("pause");
+	system("cls");
 }
